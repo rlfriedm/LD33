1aea36b1e70b0f60f4ea76cc99247776cbcea8d1
Rachel Friedman <rlfriedm@hamilton.edu>
2015-08-22 12:28:29 -0700

    initial commit, basic character controller

---
+++ LD33/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public int walkSpeed;
+	private bool grounded = false;
+
+	// Use this for initialization
+	void Start () {
+	 // add animator here
+	}
+	
+	void FixedUpdate () {
+		if ((Input.GetKey("up") || Input.GetKey("space")) && grounded) {
+			gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 200));
+			grounded = false;
+		}
+		else if (Input.GetKey("right")) {
+			transform.Translate(Vector3.right * walkSpeed * Time.deltaTime);
+		}
+		else if (Input.GetKey("left")) {
+			transform.Translate (Vector3.left * walkSpeed * Time.deltaTime);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.name == "Ground") {
+			grounded = true;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.name == "Person") {
+			print("person");
+		}
+	}
+}
